ad6d53ff37ed54076a4a4fef0319e5153678bcfe
jaiswal09 <abhitanya2831@gmail.com>
2026-01-18 18:43:12 +0530

    new line with java

--- FirstClass.java
+++ FirstClass.java
@@ -1,5 +1,6 @@
 class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.print("Hello, World!\n");
+        System.out.print("From Java");
     }
 }
