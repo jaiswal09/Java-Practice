e093097b9b0ba0dd8ea306e59af7d18aa4f85f12
jaiswal09 <abhitanya2831@gmail.com>
2026-01-18 19:10:40 +0530

    Variables

---
+++ Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+public static void main(String[] args) {
+    String Output = "The perimeter of quadrilateral is: ";
+    int side1 = 5;
+    int side2 = 10;
+
+    System.out.println(Output);
+
+    System.out.println( (side1 + side2) * 2);
+}
+    
+}
