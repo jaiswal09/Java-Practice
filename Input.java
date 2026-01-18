390f83a344b0659c6317101b9f2e38a29aa8d3ca
jaiswal09 <abhitanya2831@gmail.com>
2026-01-18 19:20:04 +0530

    Input from user

---
+++ Input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter your name: ");
+            String name = scanner.nextLine();
+            System.out.println("Hello, " + name + "!");
+        }
+
+        
+    }
+}
